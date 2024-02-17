5a96bcedd2f7f3602df227cdfbd37e34c90a81d5
Aditya <as2397@hw.ac.uk>
2024-02-17 14:51:19 +0400

    Created testmain for testing load_image

---
+++ testmain.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "process.c"
+
+int main()
+{
+    const char *filename = "";
+    struct Image *img = load_image(filename);
+
+    if (img == NULL)
+    {
+        printf("Failed to load image from file %s\n", filename);
+        return 1;
+    }
+
+    printf("Image loaded successfully with dimensions: %d x %d\n", img->width, img->height);
+
+    free_image(img);
+    return 0;
+}
