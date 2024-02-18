78f83de4a7f02f4ec9374c9d268a699550558d43
Aditya <as2397@hw.ac.uk>
2024-02-18 21:07:54 +0400

    Updated testprocess as base for implemeting bonus task

--- testprocess.c
+++ testprocess.c
@@ -9,9 +9,6 @@
 #include <string.h>
 #include <time.h>
 
-#define MIN(x, y) (((x) < (y)) ? (x) : (y))
-#define MAX(x, y) (((x) > (y)) ? (x) : (y))
-
 /* The RGB values of a pixel. */
 struct Pixel
 {
@@ -173,31 +170,16 @@ struct Image *copy_image(const struct Image *source)
  * (TODO: Write a better comment here, and rename the function.
  * You may need to add or change arguments depending on the task.)
  * Returns a new struct Image containing the result, or NULL on error. */
-struct Image *apply_NOISE(struct Image *source, int noise_strength)
+struct Image *apply_NOISE(const struct Image *source, int noise_strength)
 {
-    // Create a copy of the original image
     struct Image *copy = copy_image(source);
     if (copy == NULL)
     {
+        fprintf(stderr, "Error copying image to apply Noise");
         return NULL;
     }
 
-    // Seed the random number generator with the current time
     srand(time(NULL));
-
-    // Iterate over all pixels in the image
-    // for (int i = 0; i < copy->width * copy->height; i++)
-    // {
-    //     // Add random noise to each color component
-    //     copy->pixels[i].red += rand() % (noise_strength * 2 + 1) - noise_strength;
-    //     copy->pixels[i].green += rand() % (noise_strength * 2 + 1) - noise_strength;
-    //     copy->pixels[i].blue += rand() % (noise_strength * 2 + 1) - noise_strength;
-
-    //     // Clamp the color components to the valid range (0-65535)
-    //     copy->pixels[i].red = copy->pixels[i].red < 0 ? 0 : (copy->pixels[i].red > 65535 ? 65535 : copy->pixels[i].red);
-    //     copy->pixels[i].green = copy->pixels[i].green < 0 ? 0 : (copy->pixels[i].green > 65535 ? 65535 : copy->pixels[i].green);
-    //     copy->pixels[i].blue = copy->pixels[i].blue < 0 ? 0 : (copy->pixels[i].blue > 65535 ? 65535 : copy->pixels[i].blue);
-    // }
 
     for (int i = 0; i < source->width * source->height; i++)
     {
@@ -205,8 +187,7 @@ struct Image *apply_NOISE(struct Image *source, int noise_strength)
         int noiseGreen = rand() % (2 * noise_strength + 1) - noise_strength;
         int noiseBlue = rand() % (2 * noise_strength + 1) - noise_strength;
 
-        int newRed = source->pixels[i].red + noiseRed;
-        // Make sure the noise is within the valid range (0-65535)
+        int newRed = copy->pixels[i].red + noiseRed;
         if (newRed < 0)
         {
             newRed = 0;
@@ -216,7 +197,7 @@ struct Image *apply_NOISE(struct Image *source, int noise_strength)
             newRed = 65535;
         }
 
-        int newGreen = source->pixels[i].green + noiseGreen;
+        int newGreen = copy->pixels[i].green + noiseGreen;
         if (newGreen < 0)
         {
             newGreen = 0;
@@ -226,7 +207,7 @@ struct Image *apply_NOISE(struct Image *source, int noise_strength)
             newGreen = 65535;
         }
 
-        int newBlue = source->pixels[i].blue + noiseBlue;
+        int newBlue = copy->pixels[i].blue + noiseBlue;
         if (newBlue < 0)
         {
             newBlue = 0;
@@ -240,7 +221,6 @@ struct Image *apply_NOISE(struct Image *source, int noise_strength)
         copy->pixels[i].green = (unsigned short)newGreen;
         copy->pixels[i].blue = (unsigned short)newBlue;
     }
-
     return copy;
 }
 
@@ -250,84 +230,91 @@ struct Image *apply_NOISE(struct Image *source, int noise_strength)
  * Returns true on success, or false on error. */
 bool apply_CODE(const struct Image *source)
 {
-    printf("const int image_width = %d;\n", source->width);
+    if (source == NULL)
+    {
+        printf("Error reading file \n");
+        return false;
+    }
+
+    printf("const in image_width = %d;\n", source->width);
     printf("const int image_height = %d;\n", source->height);
     printf("const struct Pixel image_data = {\n");
 
-    int row_width = (source->width * 3 * 4 + 2) / 3; // 3(R, G, B) * 4(short) + 2(spaces)
-    int i = 0;
     printf("    ");
-    for (int y = 0; y < source->height; y++)
+    for (int i = 0; i < source->width * source->height; i++)
     {
-        for (int x = 0; x < source->width; x++)
+        printf("{%hu, %hu, %hu}", source->pixels[i].red, source->pixels[i].green, source->pixels[i].blue);
+
+        if (i != source->width * source->height - 1)
         {
-            if (x > 0 && (i + 1) % row_width == 0)
-            {
-                printf("\n    ");
-            }
-            else if (x > 0)
-            {
-                printf(", ");
-            }
-            const struct Pixel *px = &source->pixels[y * source->width + x];
-            printf("{%d, %d, %d}", px->red, px->green, px->blue);
-            i++;
+            printf(", ");
         }
-        printf("\n");
-        printf("} \n");
+
+        if ((i + 1) % 3 == 0)
+        {
+            printf("\n");
+            printf("    ");
+        }
     }
+
+    printf("\n");
+    printf("} \n");
 
     return true;
 }
 
-// int main(int argc, char *argv[])
-// {
-//     /* Initialise the random number generator, using the time as the seed */
-//     /* TODO: Remove this if you don't need to use rand() */
-//     srand(time(NULL));
+int main(int argc, char *argv[])
+{
+    /* Check command-line arguments */
+    if (argc != 4)
+    {
+        fprintf(stderr, "Usage: process INPUTFILE OUTPUTFILE NOISE_STRENGTH\n");
+        return 1;
+    }
 
-//     /* Check command-line arguments */
-//     if (argc != 3)
-//     {
-//         fprintf(stderr, "Usage: process INPUTFILE OUTPUTFILE\n");
-//         return 1;
-//     }
+    /* Load the input image */
+    struct Image *in_img = load_image(argv[1]);
+    if (in_img == NULL)
+    {
+        return 1;
+    }
 
-//     /* Load the input image */
-//     struct Image *in_img = load_image(argv[1]);
-//     if (in_img == NULL)
-//     {
-//         return 1;
-//     }
+    int noise_strength;
+    if (sscanf(argv[3], "%d", &noise_strength) != 1)
+    {
+        fprintf(stderr, "Invalid noise strength: %s\n", argv[3]);
+        free_image(in_img);
+        return 1;
+    }
 
-//     /* Apply the first process */
-//     struct Image *out_img = apply_BLUR(in_img);
-//     if (out_img == NULL)
-//     {
-//         fprintf(stderr, "First process failed.\n");
-//         free_image(in_img);
-//         return 1;
-//     }
+    /* Apply the first process */
+    struct Image *out_img = apply_NOISE(in_img, noise_strength);
+    if (out_img == NULL)
+    {
+        fprintf(stderr, "First process failed.\n");
+        free_image(in_img);
+        return 1;
+    }
 
-//     /* Apply the second process */
-//     if (!apply_NORM(out_img))
-//     {
-//         fprintf(stderr, "Second process failed.\n");
-//         free_image(in_img);
-//         free_image(out_img);
-//         return 1;
-//     }
+    /* Apply the second process */
+    if (!apply_CODE(out_img))
+    {
+        fprintf(stderr, "Second process failed.\n");
+        free_image(in_img);
+        free_image(out_img);
+        return 1;
+    }
 
-//     /* Save the output image */
-//     if (!save_image(out_img, argv[2]))
-//     {
-//         fprintf(stderr, "Saving image to %s failed.\n", argv[2]);
-//         free_image(in_img);
-//         free_image(out_img);
-//         return 1;
-//     }
+    /* Save the output image */
+    if (!save_image(out_img, argv[2]))
+    {
+        fprintf(stderr, "Saving image to %s failed.\n", argv[2]);
+        free_image(in_img);
+        free_image(out_img);
+        return 1;
+    }
 
-//     free_image(in_img);
-//     free_image(out_img);
-//     return 0;
-// }
+    free_image(in_img);
+    free_image(out_img);
+    return 0;
+}
